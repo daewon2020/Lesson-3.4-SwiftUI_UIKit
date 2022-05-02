c7e6beb7ee548b2fc5f32cb9da0ced46a7c2977e
Kostya Andreev <daewon@yandex.ru>
2022-05-02 17:36:36 +0300

    Initial Commit

---
+++ Lesson 3.4 SwiftUI_UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson 3.4 SwiftUI_UIKit
+//
+//  Created by Константин Андреев on 02.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 3.4 SwiftUI_UIKit/Lesson_3_4_SwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_4_SwiftUI_UIKitApp.swift
+//  Lesson 3.4 SwiftUI_UIKit
+//
+//  Created by Константин Андреев on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_4_SwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
